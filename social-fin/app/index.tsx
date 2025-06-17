3bac499ec3ffdd039f1b482576a31c63600e66c6
vsuortiz-cmu-S25 <vsuortiz1@gmail.com>
2025-06-17 00:57:03 -0700

    fix white status bars

--- social-fin/app/index.tsx
+++ social-fin/app/index.tsx
@@ -1,10 +1,13 @@
 import AuthScreen from "./screens/auth/AuthScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+
 export default function Index() {
   return (
+    <>
     <SafeAreaProvider>
-       <AuthScreen />
+      <AuthScreen />
     </SafeAreaProvider>
+    </>
   )
 }
